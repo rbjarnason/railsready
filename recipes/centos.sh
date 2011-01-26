77d22fce2e4054241272d7553692202134c3db79
Josh Frye <joshfng@gmail.com>
2011-01-25 20:08:25 -0500

    Mock up the process

--- recipes/centos.sh
+++ recipes/centos.sh
@@ -16,3 +16,20 @@ whichRuby=$6
 script_runner=$(whoami)
 railsready_path=$7
 log_file=$8
+
+#update system
+sudo yum update -y
+
+#build tools
+sudo yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel bash curl git
+
+#build git from source
+#download
+#extract and cd
+#./configure
+#make
+#make install
+#source ~/bash_profile && source ~/.bashrc
+
+#imagemagic
+sudo yum install -y ImageMagick
